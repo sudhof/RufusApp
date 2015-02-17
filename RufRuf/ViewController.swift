90e87bedca861cc852a395c054e8481714272157
Moritz Sudhof <moritz.sudhof@gmail.com>
2015-02-16 16:12:21 -0800

    iphone UI

--- RufRuf/ViewController.swift
+++ RufRuf/ViewController.swift
@@ -10,17 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var questionTxtLabel : UILabel!
-    @IBOutlet var feedbackLabel : UILabel!
+    @IBOutlet var questionTxtLabel : RDLabel!
+    @IBOutlet var feedbackLabel : RDLabel!
     @IBOutlet var optAButton : UIButton!
     @IBOutlet var optBButton : UIButton!
     @IBOutlet var refreshButton : UIButton!
+    @IBOutlet weak var bannerImg: UIImageView!
     
     let qManager = QuestionManagerModel()
+    
+    var banner_imgs = [UIImage(named: "banner1"), UIImage(named: "banner2"), UIImage(named: "banner3"), UIImage(named: "banner4"), UIImage(named: "banner5"), UIImage(named: "banner6"), UIImage(named: "banner7"), UIImage(named: "banner8"), UIImage(named: "banner9")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.questionTxtLabel.layer.cornerRadius = 5
+        self.questionTxtLabel.clipsToBounds = true
+        self.feedbackLabel.layer.cornerRadius = 5
+        self.feedbackLabel.clipsToBounds = true
+        self.optAButton.layer.cornerRadius = 5
+        self.optBButton.layer.cornerRadius = 5
+        self.refreshButton.layer.cornerRadius = 5
         refreshUI()
     }
 
@@ -38,6 +48,17 @@ class ViewController: UIViewController {
                 self.optAButton.setTitle(options[0], forState: UIControlState.Normal)
                 self.optBButton.setTitle(options[1], forState: UIControlState.Normal)
                 self.feedbackLabel.text = ""
+                self.feedbackLabel.hidden = true
+                self.optAButton.hidden = false
+                self.optBButton.hidden = false
+                
+                // Set a random banner img
+                var randomIndex = Int(arc4random_uniform(UInt32(self.banner_imgs.count)))
+                self.bannerImg.image = self.banner_imgs[randomIndex]
+                
+            } else {
+                print("ERRRRRRR")
+                print(error)
             }
         }
         
@@ -67,12 +88,30 @@ class ViewController: UIViewController {
     
     
     func evaluateResponse(chosenOptionIndex: Int) {
+        // Set a random banner img
+        var randomIndex = Int(arc4random_uniform(UInt32(self.banner_imgs.count)))
+        self.bannerImg.image = self.banner_imgs[randomIndex]
+
         feedbackLabel.text = qManager.getFeedbackText(chosenOptionIndex)
+        self.feedbackLabel.hidden = false
+        self.optAButton.hidden = true
+        self.optBButton.hidden = true
     }
     
     
-    
-    
-    
 }
+
+
+
+class RDLabel: UILabel {
+    
+    override func drawTextInRect(rect: CGRect) {
+        //let newRect = CGRectOffset(rect, 10, 0) // move text 10 points to the right
+        //super.drawTextInRect(newRect)
+        var insets: UIEdgeInsets = UIEdgeInsets(top: 0.0, left: 10.0, bottom: 0.0, right: 10.0)
+        super.drawTextInRect(UIEdgeInsetsInsetRect(rect, insets))
+    }
+}
+
+
 
